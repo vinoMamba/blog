0a524d248677f34211ca3d23120baf5ca41a1a6f
vino <vino0908@outlook.com>
2025-03-26 10:17:07 +0800

    update layout

--- app/admin/notion/sync/me/page.jsx
+++ app/admin/notion/sync/me/page.jsx
@@ -5,7 +5,7 @@ import { getAllBlocks } from "@/db"
 export default async function SyncPage() {
   const list = await getAllBlocks()
   return (
-    <main className="flex flex-col max-w-screen-sm justify-center h-screen w-screen gap-2 mx-auto">
+    <main className="flex flex-col max-w-screen-sm h-screen w-screen gap-2 mx-auto mt-20">
       <Dashboard data={list} />
     </main>
   )
